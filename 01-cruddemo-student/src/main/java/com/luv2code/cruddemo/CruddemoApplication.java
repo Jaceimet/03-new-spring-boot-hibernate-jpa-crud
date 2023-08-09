6fb8472c7bbcecd1d40a1b9205c61d3953b72b04
Jaceimet <84209692+Jaceimet@users.noreply.github.com>
2023-08-08 22:36:13 -0700

    Saving a java object with JPA
    Saved a student object to the database

--- 01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.luv2code.cruddemo;
 
+import com.luv2code.cruddemo.dao.StudentDAO;
+import com.luv2code.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,9 +13,27 @@ public class CruddemoApplication {
 	public static void main(String[] args) {SpringApplication.run(CruddemoApplication.class, args);}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args){
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 		return runner -> {
-			System.out.println("Hello World");
+			createStudent(studentDAO);
 		};
 	}
+
+	private void createStudent(StudentDAO studentDAO) {
+
+		// create the student object
+		System.out.println("Creating new student object ...");
+		Student tempStudent = new Student("Paul", "Doe", "paul@luv2code.com");
+
+		// save the student object
+		System.out.println("Saving the student .... ");
+		studentDAO.save(tempStudent);
+
+		// display the id of the saved student
+		System.out.println("Save student. Generate id: " + tempStudent.getId());
+	}
 }
+
+
+
+
