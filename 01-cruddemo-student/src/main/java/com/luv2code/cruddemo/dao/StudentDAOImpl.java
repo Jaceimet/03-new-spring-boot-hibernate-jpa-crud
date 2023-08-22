23e47afb4567581a7bfd782fa2c7fd9d21031c55
Jaceimet <84209692+Jaceimet@users.noreply.github.com>
2023-08-21 21:44:21 -0700

    Read multiple students from the database
    read multiple students and display data in the console

--- 01-cruddemo-student/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
+++ 01-cruddemo-student/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
@@ -3,9 +3,12 @@ package com.luv2code.cruddemo.dao;
 
 import com.luv2code.cruddemo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO {
@@ -34,4 +37,17 @@ public class StudentDAOImpl implements StudentDAO {
     public Student findById(Integer id) {
         return entityManager.find(Student.class,id);
     }
+
+    @Override
+    public List<Student> findAll() {
+
+        // create query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student", Student.class);
+
+        // return results
+        return theQuery.getResultList();
+    }
+
+
+
 }
